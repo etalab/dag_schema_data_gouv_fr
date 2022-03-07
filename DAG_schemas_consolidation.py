81769576353df25db20dbf4d14a6a12774cd6d01
Sixte de Maupeou <sixtedemaupeou@gmail.com>
2022-03-07 18:17:26 +0100

    Split schemas_consolidation.ipynb into consolidation and upload notebooks. Add fix on coordinates order for schema IRVE

--- DAG_schemas_consolidation.py
+++ DAG_schemas_consolidation.py
@@ -1,13 +1,17 @@
 from airflow.models import DAG, Variable
+from operators.papermill import PapermillOperator
 from operators.papermill_minio import PapermillMinioOperator
 from airflow.operators.python import PythonOperator
 from operators.clean_folder import CleanFolderOperator
 from operators.mattermost import MattermostOperator
 from airflow.utils.dates import days_ago
-from datetime import timedelta, datetime
+from datetime import timedelta
+import os
 import requests
 from minio import Minio
 import pandas as pd
+
+from dag_schema_data_gouv_fr.utils.geo import fix_coordinates_order
 
 AIRFLOW_DAG_HOME='/opt/airflow/dags/'
 TMP_FOLDER='/tmp/'
@@ -19,19 +23,20 @@ MINIO_BUCKET = Variable.get("minio_bucket_opendata")
 MINIO_USER = Variable.get("secret_minio_user_opendata")
 MINIO_PASSWORD = Variable.get("secret_minio_password_opendata")
 
-MATTERMOST_ENDPOINT = Variable.get("secret_mattermost_schema_activite")
+# MATTERMOST_ENDPOINT = Variable.get("secret_mattermost_schema_activite")
 
 SCHEMA_CATALOG = 'https://schema.data.gouv.fr/schemas/schemas.json'
 # SCHEMA_CATALOG = 'https://raw.githubusercontent.com/geoffreyaldebert/schema-test/master/schemas.json'
 
 API_KEY = Variable.get("secret_api_key_data_gouv")
-# API_URL = "https://demo.data.gouv.fr/api/1/"
-API_URL = "https://www.data.gouv.fr/api/1/"
+API_URL = "https://demo.data.gouv.fr/api/1/"
+# API_URL = "https://www.data.gouv.fr/api/1/"
 
 default_args = {
-   'email': ['geoffrey.aldebert@data.gouv.fr'],
+   'email': ['sixte.de-maupeou@data.gouv.fr'],
    'email_on_failure': True
 }
+
 
 def notification_synthese(**kwargs):
     templates_dict = kwargs.get("templates_dict")
@@ -90,12 +95,12 @@ def notification_synthese(**kwargs):
                 print('No report for {}'.format(s['name']))
                 pass
 
-    publish_mattermost = MattermostOperator(
-        task_id="publish_result",
-        mattermost_endpoint=MATTERMOST_ENDPOINT,
-        text=message
-    )
-    publish_mattermost.execute(dict())
+    # publish_mattermost = MattermostOperator(
+    #     task_id="publish_result",
+    #     mattermost_endpoint=MATTERMOST_ENDPOINT,
+    #     text=message
+    # )
+    # publish_mattermost.execute(dict())
 
 with DAG(
     dag_id=DAG_NAME,
@@ -110,31 +115,49 @@ with DAG(
         task_id="clean_previous_outputs",
         folder_path=TMP_FOLDER + DAG_FOLDER
     )
-    
-    run_nb_consolidation = PapermillMinioOperator(
-        task_id="run_notebook_schemas_consolidation",
-        input_nb=AIRFLOW_DAG_HOME + DAG_FOLDER + "notebooks/schemas_consolidation.ipynb",
-        output_nb='{{ ds }}' + ".ipynb",
-        tmp_path=TMP_FOLDER+DAG_FOLDER + '{{ ds }}' + "/",
-        minio_url=MINIO_URL,
-        minio_bucket=MINIO_BUCKET,
-        minio_user=MINIO_USER,
-        minio_password=MINIO_PASSWORD,
-        minio_output_filepath='datagouv/schemas_consolidation/' + '{{ ds }}' + "/",
-        parameters={
+
+    tmp_folder = TMP_FOLDER + DAG_FOLDER + '{{ ds }}' + "/"
+
+    shared_notebooks_params = {
             "msgs": "Ran from Airflow " + '{{ ds }}' + "!",
             "WORKING_DIR": AIRFLOW_DAG_HOME + DAG_FOLDER + 'notebooks/',
-            "TMP_FOLDER": TMP_FOLDER + DAG_FOLDER + '{{ ds }}' + "/",
-            "OUTPUT_DATA_FOLDER": TMP_FOLDER + DAG_FOLDER + '{{ ds }}' + "/output/",
+            "TMP_FOLDER": tmp_folder,
             "API_KEY": API_KEY,
             "API_URL": API_URL,
             "DATE_AIRFLOW": '{{ ds }}',
             "SCHEMA_CATALOG": SCHEMA_CATALOG
         }
+    
+    run_nb_consolidation = PapermillOperator(
+        task_id="run_notebook_schemas_consolidation",
+        input_nb=AIRFLOW_DAG_HOME + DAG_FOLDER + "notebooks/schemas_consolidation/schemas_consolidation.ipynb",
+        output_nb='{{ ds }}' + "_schemas_consolidation.ipynb",
+        parameters=shared_notebooks_params
+    )
+
+    improve_datasets_quality = PythonOperator(
+        task_id="improve_datasets_quality",
+        python_callable=lambda: fix_coordinates_order(os.listdir(os.path.join(tmp_folder, 'consolidated_data', 'etalab_schema-irve')))
+    )
+
+    upload_consolidation = PapermillMinioOperator(
+        task_id="upload_consolidated_datasets",
+        input_nb=AIRFLOW_DAG_HOME + DAG_FOLDER + "notebooks/schemas_consolidation/consolidation_upload.ipynb",
+        output_nb='{{ ds }}' + "_consolidation_upload.ipynb",
+        tmp_path=TMP_FOLDER+DAG_FOLDER + '{{ ds }}' + "/",
+        minio_url=MINIO_URL,
+        minio_bucket=MINIO_BUCKET,
+        minio_user=MINIO_USER,
+        minio_password=MINIO_PASSWORD,
+        minio_output_filepath='datagouv_test/schemas_consolidation/' + '{{ ds }}' + "/",
+        parameters={
+            **shared_notebooks_params,
+            "OUTPUT_DATA_FOLDER": TMP_FOLDER + DAG_FOLDER + '{{ ds }}' + "/output/"
+        }
     )
 
     notification_synthese = PythonOperator(
-        task_id="notification_synthese", 
+        task_id="notification_synthese",
         python_callable=notification_synthese,
         templates_dict={
             "TODAY": '{{ ds }}'
@@ -143,4 +166,4 @@ with DAG(
     
 
     
-    clean_previous_outputs >> run_nb_consolidation >> notification_synthese
+    clean_previous_outputs >> run_nb_consolidation >> improve_datasets_quality >> upload_consolidation >> notification_synthese
---
+++ utils/geo.py
@@ -0,0 +1,41 @@
+from typing import List
+import geojson
+import json
+import pandas as pd
+from shapely.geometry import Point, shape
+from shapely.geometry.polygon import Polygon
+
+with open("/opt/airflow/dags/dag_schema_data_gouv_fr/utils/france_bbox.geojson") as f:
+    FRANCE_BBOXES = geojson.load(f)
+
+def is_point_in_polygon(x: float, y: float, polygon: List[List[float]]) -> bool:
+    point = Point(x, y)
+    polygon_shape = Polygon(polygon)
+    return polygon_shape.contains(point)
+
+
+def is_point_in_france(coordonnees_xy: List[float]) -> bool:
+    p = Point(*coordonnees_xy)
+    
+    # Create a Polygon
+    geoms = [region["geometry"] for region in FRANCE_BBOXES.get('features')]
+    polys = [shape(geom) for geom in geoms]
+    return any([p.within(poly) for poly in polys])
+
+
+def fix_coordinates_order(filepaths: List[str], coordinates_column: str="coordonneesXY") -> None:
+    """
+    Cette fonction modifie un fichier CSV pour placer la longitude avant la lattitude
+    dans la colonne qui contient les deux au format "[lon, lat]".
+    """
+
+    def fix_coordinates(row: pd.Series) -> pd.Series:
+        coordonnees_xy = json.loads(row[coordinates_column])
+        reversed_coordonnees = list(reversed(coordonnees_xy))
+        if is_point_in_france(reversed_coordonnees):
+            # Coordinates are inverted with lat before lon
+            row[coordinates_column] = json.dumps(reversed_coordonnees)
+        return row
+
+    for filepath in filepaths:
+        pd.read_csv(filepath).apply(fix_coordinates, axis=1).to_csv(filepath)
